d6759d8f13a0dc875e93163de020c7adf57f8648
Qianqian Zhu <zhuqianqian.299@gmail.com>
2016-11-15 13:49:56 +0800

    Ugly Number II

--- C/uglyNumber.c
+++ C/uglyNumber.c
@@ -1,5 +1,6 @@
 /*
  * https://leetcode.com/problems/ugly-number/
+ * https://leetcode.com/problems/ugly-number-ii/
  */
  
 bool isUgly(int num) {
@@ -15,4 +16,33 @@ bool isUgly(int num) {
         }
     }
     return true;
+}
+
+/* Ugly Number II */
+
+int min(int a, int b, int c) {
+    if (a < b) {
+        if (a < c) return a;
+        return c;
+    }
+    if (b > c) return c;
+    return b;
+}
+
+int nthUglyNumber(int n) {
+    if (n < 0) return 0;
+    int* ugly = (int*)malloc(sizeof(int)*(n + 1));
+    ugly[1] = 1;
+    int i, ans;
+    int x[3] = { 1,1,1 };
+    int n2 = 2, n3 = 3, n5 = 5;
+    for (i = 2; i <= n; i++) {
+        ugly[i] = min(n2, n3, n5);
+        if (ugly[i] == n2) n2 = ugly[++x[0]] * 2;
+        if (ugly[i] == n3) n3 = ugly[++x[1]] * 3;
+        if (ugly[i] == n5) n5 = ugly[++x[2]] * 5;
+    }
+    ans = ugly[n];
+    free(ugly);
+    return ans;
 }
