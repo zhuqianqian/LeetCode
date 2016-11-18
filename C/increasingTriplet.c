13700e56187debae96091dd54e504e9b0469683b
Qianqian Zhu <zhuqianqian.299@gmail.com>
2016-11-18 18:15:50 +0800

    Increasing Tripet Subsequence

--- C/increasingTriplet.c
+++ C/increasingTriplet.c
@@ -1,9 +1,14 @@
 /**
- * https://leetcode.com/problems/increasing-triplet-subsequence/
- */
-
-#include <stdbool.h>
+* https://leetcode.com/problems/increasing-triplet-subsequence/
+*/
 
 bool increasingTriplet(int* nums, int numsSize) {
-
+    int first, second, i;
+    first = second = INT_MAX;
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] <= first) { first = nums[i]; }
+        else if (nums[i] <= second) { second = nums[i]; }
+        else { return true; }
+    }
+    return false;
 }
